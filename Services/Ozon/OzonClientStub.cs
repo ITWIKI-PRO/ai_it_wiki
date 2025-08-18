9888ac1b0b074188807015d8fa6852192e28427c
Pycekursk <78217475+Pycekursk@users.noreply.github.com>
2025-08-18 22:34:56 +0300

    Унификация SKU и DI в LLMOpenAPIController

--- Services/Ozon/OzonClientStub.cs
+++ Services/Ozon/OzonClientStub.cs
@@ -5,15 +5,15 @@ namespace ai_it_wiki.Services.Ozon
 {
     public class OzonClientStub : IOzonClient
     {
-        public Task<int> GetRatingAsync(long sku)
+        public Task<int> GetRatingAsync(string sku)
         {
-            // TODO: запрос к Ozon API для получения рейтинга
+            // TODO[critical]: реализовать запрос к Ozon API для получения рейтинга
             return Task.FromResult(0);
         }
 
-        public Task UpdateCardAsync(long sku)
+        public Task UpdateCardAsync(string sku)
         {
-            // TODO: обновление карточки товара через Ozon API
+            // TODO[critical]: реализовать обновление карточки товара через Ozon API
             return Task.CompletedTask;
         }
     }
